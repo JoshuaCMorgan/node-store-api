8db02be2764feacc607abedf0694a92ff3d306fc
Joshua Morgan <jmorg0605@gmail.com>
2023-02-11 13:03:49 +0800

    add pagination functionality

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
   // https://mongoosejs.com/docs/queries.html
-  const products = await Product.find({}).limit(1);
+  const products = await Product.find({}).sort('name').select('name price');
 
   res.status(200).json({
     products,
@@ -11,7 +11,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields, limit } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -44,11 +44,11 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldsList);
   }
 
-  // limit
-   if (limit) {
-    result = result.limit(Number(limit))
-   }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
 
+  result = result.skip(skip).limit(limit);
   const products = await result;
 
   res.status(200).json({
