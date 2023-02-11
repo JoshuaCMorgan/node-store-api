2ccfa759f9a70f71245106b94d1680134252a9e2
Joshua Morgan <jmorg0605@gmail.com>
2023-02-11 09:07:25 +0800

    initial sort method added

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,8 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'ab';
-
-  const products = await Product.find({
-    name: { $regex: search, $options: 'i' },
-  });
+  // https://mongoosejs.com/docs/queries.html
+  const products = await Product.find({}).sort('-name, price');
 
   res.status(200).json({
     products,
@@ -14,16 +11,19 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
   }
+
   if (company) {
     queryObject.company = company;
   }
+  
   if (name) {
+    // https://www.mongodb.com/docs/manual/reference/operator/query/regex/#mongodb-query-op.-regex
     queryObject.name = { $regex: name, $options: 'i' };
   }
 
